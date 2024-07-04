af233cdf7517b273de61c83819c001dee371129a
HeyItsMeBJ <bhupeshjain3221@gmail.com>
2024-07-05 02:31:42 +0530

    added portfolio project

--- src/components/Projects/Menu.jsx
+++ src/components/Projects/Menu.jsx
@@ -1,11 +1,17 @@
 import project1 from "../../assets/aviator-logo.jpg";
 import project2 from "../../assets/yt-logo.jpg";
 import project3 from "../../assets/blog-logo.jpg";
-// import project4 from "../../assets/project4.jpg";
+import project4 from "../../assets/portfolio-logo.png";
 // import project6 from "../../assets/project6.jpg";
 
-
 const Menu = [
+  {
+    id: 4,
+    url: "https://github.com/HeyItsMeBJ/porfolio",
+    image: project4,
+    title: "Portfolio | React Js",
+    category: "Frontend",
+  },
   {
     id: 1,
     url: "",
@@ -18,24 +24,18 @@ const Menu = [
     id: 2,
     url: "https://github.com/HeyItsMeBJ/yt_backend",
     image: project2,
-    title: "YTube | Robust backend system for a YouTube-like platform | Node Js + Express Js + MongoDB ",
+    title:
+      "YTube | Robust backend system for a YouTube-like platform | Node Js + Express Js + MongoDB ",
     category: "Backend",
   },
   {
     id: 3,
     url: "https://github.com/HeyItsMeBJ/miniBlog_react_appwrite",
     image: project3,
-    title: "MINI Blog Site | React + TailwindCSS + Appwrite",
+    title: "MINI Blog Site | React Js + TailwindCSS + Appwrite",
     category: "Frontend",
   },
 
-  // {
-  //   id: 4,
-  //   url: "https://github.com/horcruxxxx/Secrests",
-  //   image: project4,
-  //   title: "Secrets | Node.js + Express.js + Oauth + Passport.js",
-  //   category: "Backend",
-  // },
   // ,
   // {
   //   id: 5,
